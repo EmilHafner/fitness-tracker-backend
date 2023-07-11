37e3af907039e9513682a6a08627bdd3a48e371b
Aronlukas <hafner.emil@gmail.com>
2023-07-11 19:22:19 +0200

    changes to testController only for testing purposes

--- src/main/java/com/example/fitnesstrackerbackend/test/testController.java
+++ src/main/java/com/example/fitnesstrackerbackend/test/testController.java
@@ -1,16 +1,23 @@
 package com.example.fitnesstrackerbackend.test;
 
+import com.example.fitnesstrackerbackend.service.UserService;
+import com.example.fitnesstrackerbackend.user.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/test-controller")
 public class testController {
 
+  UserService userService;
+
   @GetMapping
-  public ResponseEntity<String> helloWorld() {
-    return ResponseEntity.ok("Hello World. Sent from secured endpoint");
+  public List<User> helloWorld() {
+    return userService.getAllUsers();
   }
 }
