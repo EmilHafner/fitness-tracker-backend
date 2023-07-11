ec568dd6c51b670da2f2775ce6b75ed802136085
Aronlukas <hafner.emil@gmail.com>
2023-07-11 19:20:14 +0200

    Added Cors for the local frontend and made endpoint /api/v*/admin/** only accessible to admins

--- src/main/java/com/example/fitnesstrackerbackend/config/SecurityConfig.java
+++ src/main/java/com/example/fitnesstrackerbackend/config/SecurityConfig.java
@@ -4,12 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +30,12 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
             .csrf(AbstractHttpConfigurer::disable)
+            .cors(Customizer.withDefaults()) // By default, uses a Bean by the name of corsConfigurationSource
             .authorizeHttpRequests(
                     (auth) -> auth.requestMatchers("/api/v*/auth/**").permitAll())
+            .authorizeHttpRequests(
+                    (auth) -> auth.requestMatchers("/api/v*/admin/**").hasRole("ADMIN")
+            )
             .authorizeHttpRequests(
                     (auth) -> auth.anyRequest().authenticated())
             .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -32,5 +43,16 @@ public class SecurityConfig {
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
   }
+
+  @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://192.168.0.52:3000"));
+        configuration.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
