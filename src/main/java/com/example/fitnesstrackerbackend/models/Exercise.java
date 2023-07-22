596d8fc8d443068aba071b899b5bbe7e018394e7
Aronlukas <hafner.emil@gmail.com>
2023-07-22 16:35:05 +0200

    Added Exercise Entity

--- src/main/java/com/example/fitnesstrackerbackend/models/Exercise.java
+++ src/main/java/com/example/fitnesstrackerbackend/models/Exercise.java
@@ -1,10 +1,12 @@
 package com.example.fitnesstrackerbackend.models;
 
+import com.example.fitnesstrackerbackend.models.enums.Bodypart;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
+// Entity class for gym exercises
 @Data
 @Entity
 @AllArgsConstructor
@@ -14,6 +16,6 @@ import lombok.*;
 public class Exercise {
     @Id
     private Integer id;
-
-
+    private String name;
+    private Bodypart bodypart;
 }
---
+++ src/main/java/com/example/fitnesstrackerbackend/models/enums/Bodypart.java
@@ -0,0 +1,12 @@
+package com.example.fitnesstrackerbackend.models.enums;
+
+public enum Bodypart {
+    CHEST,
+    BACK,
+    SHOULDERS,
+    ARMS,
+    LEGS,
+    ABS,
+    CARDIO
+
+}
