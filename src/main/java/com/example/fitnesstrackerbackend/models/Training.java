ee2185b1ca5be9071f7815a84a01e7e17e9688ed
Aronlukas <hafner.emil@gmail.com>
2023-07-30 00:25:55 +0200

    Training now has a oneToMany relation to exerciseEvents and an @JsonIgnore on the user.

--- src/main/java/com/example/fitnesstrackerbackend/models/Training.java
+++ src/main/java/com/example/fitnesstrackerbackend/models/Training.java
@@ -1,26 +1,29 @@
 package com.example.fitnesstrackerbackend.models;
 
 import com.example.fitnesstrackerbackend.user.User;
-import jakarta.persistence.Column;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 
-import java.sql.Time;
 import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
-@Data
+@Getter @Setter @RequiredArgsConstructor
 @Entity
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "trainings")
 public class Training {
   @Id
@@ -28,9 +31,38 @@ public class Training {
   private Long id;
   @ManyToOne
   @JoinColumn(nullable = false)
+  @JsonIgnore
   private User user;
   private Date startDateTime;
   private Date endDateTime;
-  @ManyToOne @JoinColumn(name = "exercise_in_training_id")
-  private ExerciseInTraining exerciseInTraining;
+
+  @OneToMany(mappedBy = "training")
+  private List<ExerciseEvent> exerciseEvents;
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this
+            == o) return true;
+    if (o
+            == null) return false;
+    Class<?> oEffectiveClass = o instanceof HibernateProxy ?
+            ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() :
+            o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy ?
+            ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() :
+            this.getClass();
+    if (thisEffectiveClass
+            != oEffectiveClass) return false;
+    Training training = (Training) o;
+    return getId()
+            != null
+            && Objects.equals(getId(), training.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy ?
+            ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() :
+            getClass().hashCode();
+  }
 }
