5a6b7e168cd0504d4cd8591c39c308c0f0444e16
Aronlukas <hafner.emil@gmail.com>
2023-07-30 15:09:49 +0200

    Renamed exerciseTypeEndpoint from /exercises to /exerciseTypes

--- src/main/java/com/example/fitnesstrackerbackend/controller/ExerciseTypeController.java
+++ src/main/java/com/example/fitnesstrackerbackend/controller/ExerciseTypeController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/exercises")
+@RequestMapping("/api/v1/exerciseTypes")
 public class ExerciseTypeController {
 
   private final ExerciseTypeService exerciseTypeService;
