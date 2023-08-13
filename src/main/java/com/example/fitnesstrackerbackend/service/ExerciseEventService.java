fc2949f853b8f66f34d0c321c03bc95ecbf7abb9
Aronlukas <hafner.emil@gmail.com>
2023-08-13 20:51:16 +0200

    Added Method Security to ExerciseEventService

--- src/main/java/com/example/fitnesstrackerbackend/service/ExerciseEventService.java
+++ src/main/java/com/example/fitnesstrackerbackend/service/ExerciseEventService.java
@@ -9,6 +9,8 @@ import com.example.fitnesstrackerbackend.repository.ExerciseTypeRepository;
 import com.example.fitnesstrackerbackend.repository.TrainingsSetRepository;
 import com.example.fitnesstrackerbackend.service.validators.AccessValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,26 +25,31 @@ public class ExerciseEventService {
   private final AccessValidator accessValidator;
   private final ExerciseTypeRepository exerciseTypeRepository;
 
+  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+  @PostAuthorize("returnObject?.training.user.id == authentication.principal.id")
   public ExerciseEvent getExerciseEventById(Long id) {
     return exerciseEventRepository.findById(id).orElse(null);
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN') and #exerciseEvent.training.user.id == authentication.principal.id")
   public ExerciseEvent update(ExerciseEvent exerciseEvent) {
     return exerciseEventRepository.save(exerciseEvent);
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN') and @exerciseEventRepository.findById(#id).orElse(null)?.training.user.id == authentication.principal.id")
   public long deleteById(long id) {
     exerciseEventRepository.deleteById(id);
     return id;
   }
 
   // Training Set endpoints
-
+  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN') and @exerciseEventRepository.findById(#exerciseEventId).orElse(null)?.training.user.id == authentication.principal.id")
   public List<TrainingsSet> getSetsForExerciseEvent(Long exerciseEventId) throws NotFoundException {
     return exerciseEventRepository.findById(exerciseEventId).orElseThrow(
             () -> new NotFoundException(String.format("ExerciseEvent with Id %s not found", exerciseEventId))).getTrainingsSets();
   }
 
+  @PreAuthorize("hasRole('ROLE_USER') and @exerciseEventRepository.findById(#exerciseEventId).orElse(null)?.training.user.id == authentication.principal.id")
   public TrainingsSet addSetToExerciseEvent(Long exerciseEventId, TrainingsSet trainingsSet) throws NotFoundException {
     ExerciseEvent exerciseEvent = exerciseEventRepository.findById(exerciseEventId).orElseThrow(
             () -> new NotFoundException(String.format("ExerciseEvent with Id %s not found", exerciseEventId)));
@@ -56,6 +63,7 @@ public class ExerciseEventService {
   }
 
 
+  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN') and @exerciseEventRepository.findById(#exerciseEventId).orElse(null)?.training.user.id == authentication.principal.id")
   public ExerciseEvent updateExerciseTypeForExerciseEvent(Long exerciseEventId, Long exerciseTypeId) throws NotFoundException {
 
     ExerciseEvent exerciseEvent = exerciseEventRepository.findById(exerciseEventId).orElseThrow(
