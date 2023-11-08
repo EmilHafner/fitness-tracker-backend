ae9c26e18def1297d9a19cd9c6cbdf5fb262dd81
Aronlukas <hafner.emil@gmail.com>
2023-11-09 00:10:58 +0100

    Added Repository Decorator to ExerciseEventRepository

--- src/main/java/com/example/fitnesstrackerbackend/repository/ExerciseEventRepository.java
+++ src/main/java/com/example/fitnesstrackerbackend/repository/ExerciseEventRepository.java
@@ -1,11 +1,12 @@
 package com.example.fitnesstrackerbackend.repository;
 
 import com.example.fitnesstrackerbackend.models.ExerciseEvent;
-import com.example.fitnesstrackerbackend.models.TrainingsSet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ExerciseEventRepository extends JpaRepository<ExerciseEvent, Long> {
 
 }
